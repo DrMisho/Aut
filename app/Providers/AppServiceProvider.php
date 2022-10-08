c26b3488f08f282d5b9f469b659fc02bc6d24602
DrMisho <mstafa.misho@gmail.com>
2022-10-08 09:27:50 +0300

    fixing errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-<<<<<<< HEAD
 use App\View\Components\CompetitionItem;
-=======
->>>>>>> 2acf874ffd363b05bdb4b074a785ff85807be72a
 use App\View\Components\Suite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
@@ -33,9 +30,7 @@ class AppServiceProvider extends ServiceProvider
 //            'news' => 'Aut\News\Models\News',
 //        ]);
         \Blade::component('suite', Suite::class);
-<<<<<<< HEAD
         \Blade::component('competition-item', CompetitionItem::class);
-=======
->>>>>>> 2acf874ffd363b05bdb4b074a785ff85807be72a
+
     }
 }
